9bbfbfd8dacfc719c4ca3a3032f911e5ca7461ed
Dejan Ognjenović <dognjen@yandex.com>
2017-12-29 14:32:42 +0100

    Added rest application.

---
+++ jms/src/main/java/com/kumuluz/ee/samples/jms/CustomerApplication.java
@@ -0,0 +1,12 @@
+package com.kumuluz.ee.samples.jms;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author Dejan Ognjenović
+ * @since 2.4.0
+ */
+@ApplicationPath("v1")
+public class CustomerApplication extends Application {
+}
