64a9ceb964d8646c399855a591c5d207e8606c7f
Domen Kajdič <domen.kajdic@gmail.com>
2019-02-11 21:25:53 +0100

    Added Traced annotation

--- kumuluzee-opentracing-tutorial/gamma/src/main/java/com/kumuluz/ee/samples/opentracing/tutorial/gamma/Database.java
+++ kumuluzee-opentracing-tutorial/gamma/src/main/java/com/kumuluz/ee/samples/opentracing/tutorial/gamma/Database.java
@@ -1,10 +1,13 @@
 package com.kumuluz.ee.samples.opentracing.tutorial.gamma;
 
+import org.eclipse.microprofile.opentracing.Traced;
+
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import java.util.HashMap;
 
 @ApplicationScoped
+@Traced(value = true, operationName = "testingChangedOperationName")
 public class Database {
     private HashMap<Integer, String> data;
 
