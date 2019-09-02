be3634ee57385f91ebcacc5af4c8b88fc48b6241
Jan Meznaric <jmezna@gmail.com>
2019-09-02 09:25:55 +0200

    edited feature flags sample

---
+++ kumuluzee-feature-flags-unleash/src/main/java/com/kumuluz/ee/samples/unleash/connection/UnleashConnection.java
@@ -0,0 +1,45 @@
+/*
+ *  Copyright (c) 2014-2019 Kumuluz and/or its affiliates
+ *  and other contributors as indicated by the @author tags and
+ *  the contributor list.
+ *
+ *  Licensed under the MIT License (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  https://opensource.org/licenses/MIT
+ *
+ *  The software is provided "AS IS", WITHOUT WARRANTY OF ANY KIND, express or
+ *  implied, including but not limited to the warranties of merchantability,
+ *  fitness for a particular purpose and noninfringement. in no event shall the
+ *  authors or copyright holders be liable for any claim, damages or other
+ *  liability, whether in an action of contract, tort or otherwise, arising from,
+ *  out of or in connection with the software or the use or other dealings in the
+ *  software. See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.kumuluz.ee.samples.unleash.connection;
+
+/**
+ * This annotated class is only used if there is a more complex UnleashConfig object needed.
+ * If there is no need for such object, put the service name and unleash api URL in the config.yml
+ *
+ * @author Blaž Mrak
+ * @since 1.0.0
+ */
+/*
+import com.kumuluz.ee.feature.flags.common.annotations.FFConnection;
+import no.finn.unleash.util.UnleashConfig;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@FFConnection
+@ApplicationScoped
+public class UnleashConnection {
+    UnleashConfig config = UnleashConfig
+            .builder()
+            .unleashAPI("http://localhost:4242/api")
+            .appName("feature-flags-sample-service")
+            .build();
+}
+*/
